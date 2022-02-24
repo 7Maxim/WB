e91aeff9a24c478af983a8afe7b7fe468f37ec68
7Maxim <70209990+7Maxim@users.noreply.github.com>
2022-02-24 14:19:56 +0300

    add getGoods from db

---
+++ js/getGoods.js
@@ -0,0 +1,22 @@
+const getGoods = () => {
+
+  const links = document.querySelectorAll('.navigation-link');
+
+  const getData = () => {
+    fetch('https://getgoods-8a30d-default-rtdb.firebaseio.com/db.json')
+      .then((res) => res.json())
+      .then((data) => {
+        localStorage.setItem('goods', JSON.stringify(data));
+      })
+  }
+
+  links.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      getData();
+    })
+  })
+
+}
+
+getGoods();
